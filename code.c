50ed634617292a007925423e09275225727a3d8e
muhammadsaimmain-sudo <muhammadsaimmain@gmail.com>
2026-09-08 11:31:15 +0500

    Enhance main function with user input
    Added user input functionality to main function.

--- code.c
+++ code.c
@@ -2,6 +2,15 @@
 
 int main(){
 
-printf("gta 6 loading:::::::::::::::::::::::")
+printf("gta 6 loading:::::::::::::::::::::::");     
+int x;
 
+// taking input from user
+  
+  scanf("%d",&x) 
+// printing input from user
+printf("%d",x);
+
+  return 0
+    
 }
